756c916a0121f405990d5b38b29534611771e571
Laith <laith@ovestlabs.com.au>
2017-10-24 10:56:36 +1100

    First commit.

---
+++ Source/Win2dVisibleBounds/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Windows.UI;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+
+namespace Win2dVisibleBounds
+{
+	public sealed partial class MainPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnRegionsInvalidated(CanvasVirtualControl sender, CanvasRegionsInvalidatedEventArgs e)
+		{
+			Debug.WriteLine(e.VisibleRegion);
+
+			foreach (var r in e.InvalidatedRegions)
+			{
+				using (var ds = sender.CreateDrawingSession(r))
+				{
+					ds.Clear(Colors.DeepSkyBlue);
+				}
+			}
+		}
+	}
+}
